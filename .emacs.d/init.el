224b0a5b7f38c54bb70a86da36bdfc674231be4d
takayuki <armyofpigs@gmail.com>
2011-05-03 15:42:13 +0900

    goto-chg

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -130,6 +130,10 @@
 (define-key global-map (kbd "<f7>") 'point-undo)
 (define-key global-map (kbd "S-<f7>") 'point-redo)
 
+(require 'goto-chg)
+(define-key global-map (kbd "<f8>") 'goto-last-change)
+(define-key global-map (kbd "S-<f8>") 'goto-last-change-reverse)
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;
