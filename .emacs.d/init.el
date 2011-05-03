9d5437e1ee3529d85157ea06e8f54f7bd88e1ea2
takayuki <armyofpigs@gmail.com>
2011-05-03 15:49:22 +0900

    bm.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -134,6 +134,20 @@
 (define-key global-map (kbd "<f8>") 'goto-last-change)
 (define-key global-map (kbd "S-<f8>") 'goto-last-change-reverse)
 
+;; http://cvs.savannah.gnu.org/viewvc/*checkout*/bm/bm/bm.el
+(setq-default mb-buffer-persistence nil)
+(setq bm-restore-repository-on-load t)
+(require 'bm)
+(add-hook 'find-file-hooks 'bm-buffer-restore)
+(add-hook 'kill-buffer-hook 'bm-buffer-save)
+(add-hook 'after-save-hook 'bm-buffer-save)
+(add-hook 'after-revert-hook 'bm-buffer-restore)
+(add-hook 'vc-before-checkin-hook 'bm-buffer-save)
+(global-set-key (kbd "M-SPC") 'bm-toggle)
+(global-set-key (kbd "M-[") 'bm-previous)
+(global-set-key (kbd "M-]") 'bm-next)
+
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;
