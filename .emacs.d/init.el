e3bd116d98d5e26e480f7b05957360b2508d75b5
takayuki <armyofpigs@gmail.com>
2011-05-03 14:39:54 +0900

    auto-save

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -120,6 +120,11 @@
 (setq uniquify-ignore-bufffeers-re "*[*^]+*")
 
 
+;; http://homepage3.nifty.com/oatu/emacs/archives/auto-save-buffers.el
+(require 'auto-save-buffers)
+(run-with-idle-timer 2 t 'auto-save-buffers)
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; mines
